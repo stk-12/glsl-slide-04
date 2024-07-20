4a1e4695af1a1db0e7a29b6693c0094a3134b28d
keisuke sato <corydoras121@gmail.com>
2024-07-20 22:53:55 +0900

    first

---
+++ src/js/main.js
@@ -0,0 +1,201 @@
+import '../css/style.css'
+import * as THREE from "three";
+import GUI from "lil-gui";
+import { gsap, Circ } from "gsap";
+import vertexSource from "./shader/vertexShader.glsl?raw";
+import fragmentSource from "./shader/fragmentShader.glsl?raw";
+
+import img01 from '../images/photo01.jpg';
+import img02 from '../images/photo02.jpg';
+import img03 from '../images/photo03.jpg';
+import imgDisp1 from '../images/displacement.jpg';
+
+
+let renderer, scene, camera, fovRadian, distance, geometry, mesh;
+
+const canvas = document.querySelector("#canvas");
+
+let size = {
+  width: window.innerWidth,
+  height: window.innerHeight
+};
+
+async function init(){
+
+  // レンダラー
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(size.width, size.height);
+
+  //シーン
+  scene = new THREE.Scene();
+
+  //カメラ
+  //ウインドウとWebGL座標を一致させる
+  const fov = 45;
+  fovRadian = (fov / 2) * (Math.PI / 180); //視野角をラジアンに変換
+  distance = (size.height / 2) / Math.tan(fovRadian); //ウインドウぴったりのカメラ距離
+  camera = new THREE.PerspectiveCamera(fov, size.width / size.height, 1, distance * 2);
+  camera.position.z = distance;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+  scene.add(camera);
+
+  //コントローラー
+  // const controls = new OrbitControls(camera, canvas);
+  // controls.enableDamping = true;
+
+  //ジオメトリ
+  geometry = new THREE.PlaneGeometry(size.width, size.height, 40, 40);
+
+  //テクスチャ
+  const loader = new THREE.TextureLoader();
+  const texture01 = await loader.loadAsync(img01);
+  const texture02 = await loader.loadAsync(img02);
+  const texture03 = await loader.loadAsync(img03);
+
+  const textureDisp = await loader.loadAsync(imgDisp1);
+
+  //GLSL用データ
+  let uniforms = {
+    uTime: {
+      value: 0.0
+    },
+    uTexCurrent: {
+      value: texture01
+    },
+    uTexNext: {
+      value: texture02
+    },
+    uTexDisp: {
+      value: textureDisp
+    },
+    uResolution: {
+      value: new THREE.Vector2(size.width, size.height)
+    },
+    uTexResolution: {
+      value: new THREE.Vector2(2048, 1024)
+    },
+    uProgress: {
+      value: 0.0
+    },
+  };
+
+  //マテリアル
+  const material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: vertexSource,
+    fragmentShader: fragmentSource,
+    side: THREE.DoubleSide
+  });
+
+  //メッシュ
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // lil-guiの設定
+  const textures = {
+    displacement: textureDisp
+  };
+
+  const gui = new GUI();
+  const textureFolder = gui.addFolder('変形テクスチャ');
+  const textureUpload = document.getElementById('texture-upload');
+
+  textureFolder.add({ upload: () => { textureUpload.click(); } }, 'upload').name('変形用画像をアップロートする');
+
+  textureUpload.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const dataUrl = e.target.result;
+        loader.load(dataUrl, (texture) => {
+          uniforms.uTexDisp.value = texture;
+          textures.displacement = texture;
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
+  textureFolder.open();
+
+  //Progress
+  const tl = gsap.timeline({ repeat: -1 });
+  tl.to(uniforms.uProgress, {
+    value: 1.0,
+    duration: 0.8,
+    delay: 4,
+    ease: Circ.easeInOut,
+    onComplete: ()=>{
+      uniforms.uTexCurrent.value = texture02;
+      uniforms.uTexNext.value = texture03;
+      uniforms.uProgress.value = 0.0;
+    },
+  })
+  .to(uniforms.uProgress, {
+    value: 1.0,
+    duration: 0.8,
+    delay: 4,
+    ease: Circ.easeInOut,
+    onComplete: ()=>{
+      uniforms.uTexCurrent.value = texture03;
+      uniforms.uTexNext.value = texture01;
+      uniforms.uProgress.value = 0.0;
+    },
+  })
+  .to(uniforms.uProgress, {
+    value: 1.0,
+    duration: 0.8,
+    delay: 4,
+    ease: Circ.easeInOut,
+    onComplete: ()=>{
+      uniforms.uTexCurrent.value = texture01;
+      uniforms.uTexNext.value = texture02;
+      uniforms.uProgress.value = 0.0;
+    },
+  });
+
+
+  function animate(){
+    uniforms.uTime.value += 0.03;
+
+    // mesh.geometry.verticesNeedUpdate = true;
+    
+    //レンダリング
+    renderer.render(scene, camera);
+    // controls.update();
+    requestAnimationFrame(animate);
+  }
+  animate();
+  
+}
+
+init();
+
+
+//リサイズ
+function onWindowResize() {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  // レンダラーのサイズを修正
+  renderer.setSize(size.width, size.height);
+  // カメラのアスペクト比を修正
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+  distance = (size.height / 2) / Math.tan(fovRadian);
+  camera.position.z = distance;
+
+  mesh.material.uniforms.uResolution.value.set(size.width, size.height);
+  // const scaleX = size.width / mesh.geometry.parameters.width + 0.01;
+  // const scaleY = size.height / mesh.geometry.parameters.height + 0.01;
+  const scaleX = Math.round(size.width / mesh.geometry.parameters.width * 100) / 100 + 0.01;
+  const scaleY = Math.round(size.height / mesh.geometry.parameters.height * 100) / 100 + 0.01;
+
+  mesh.scale.set(scaleX, scaleY, 1);
+
+}
+window.addEventListener("resize", onWindowResize);
---
+++ src/js/shader/fragmentShader.glsl
@@ -0,0 +1,56 @@
+varying vec2 vUv;
+uniform float uTime;
+uniform sampler2D uTexCurrent;
+uniform sampler2D uTexNext;
+uniform sampler2D uTexDisp;
+uniform vec2 uResolution;
+uniform vec2 uTexResolution;
+uniform float uProgress;
+float parabola( float x, float k ) {
+  return pow( 4. * x * ( 1. - x ), k );
+}
+
+void main() {
+  vec2 uv = vUv;
+
+
+  // vec2 ratio = vec2(
+  //       min((resolution.x / resolution.y) / (imageResolution.x / imageResolution.y), 1.0),
+  //       min((resolution.y / resolution.x) / (imageResolution.y / imageResolution.x), 1.0)
+  //     );
+
+  //   vec2 uv = vec2(
+  //       vUv.x * ratio.x + (1.0 - ratio.x) * 0.5,
+  //       vUv.y * ratio.y + (1.0 - ratio.y) * 0.5
+  //     );
+
+  // vec2 ratio = vec2(
+  //   min((uGeoResolution.x / uGeoResolution.y) / (uTexResolution.x / uTexResolution.y), 1.0),
+  //   min((uGeoResolution.y / uGeoResolution.x) / (uTexResolution.y / uTexResolution.x), 1.0)
+  // );
+  vec2 ratio = vec2(
+    min((uResolution.x / uResolution.y) / (uTexResolution.x / uTexResolution.y), 1.0),
+    min((uResolution.y / uResolution.x) / (uTexResolution.y / uTexResolution.x), 1.0)
+  );
+
+  uv = vec2(
+    vUv.x * ratio.x + (1.0 - ratio.x) * 0.5,
+    vUv.y * ratio.y + (1.0 - ratio.y) * 0.5
+  );
+
+  vec3 texDisp = texture2D(uTexDisp, uv).rgb;
+  float disp = texDisp.r;
+  disp = disp * parabola(uProgress, 0.8);
+
+  vec2 dispUv = vec2(uv.x + disp, uv.y);
+  vec2 dispUv2 = vec2(uv.x - disp, uv.y);
+
+  // vec3 tex1 = texture2D(uTex01, uv + disp).rgb;
+  // vec3 tex2 = texture2D(uTex02, uv - disp).rgb;
+  vec3 tex1 = texture2D(uTexCurrent, dispUv).rgb;
+  vec3 tex2 = texture2D(uTexNext, dispUv2).rgb;
+
+  vec3 color = mix(tex1, tex2, uProgress);
+
+  gl_FragColor = vec4(color, 1.0);
+}
