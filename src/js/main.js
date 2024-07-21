95899e90abd0abb18d5751f64b3dd5fe8811cc2d
keisuke sato <corydoras121@gmail.com>
2024-07-21 21:28:22 +0900

    GUI設定

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,12 @@ import { gsap, Circ } from "gsap";
 import vertexSource from "./shader/vertexShader.glsl?raw";
 import fragmentSource from "./shader/fragmentShader.glsl?raw";
 
-import img01 from '../images/photo01.jpg';
-import img02 from '../images/photo02.jpg';
-import img03 from '../images/photo03.jpg';
+// import img01 from '../images/photo01.jpg';
+// import img02 from '../images/photo02.jpg';
+// import img03 from '../images/photo03.jpg';
+import img01 from '../images/01.png';
+import img02 from '../images/02.png';
+import img03 from '../images/03.png';
 import imgDisp1 from '../images/displacement.jpg';
 
 
@@ -99,6 +102,10 @@ async function init(){
   const textures = {
     displacement: textureDisp
   };
+  const settings = {
+    duration: 1.8,
+    delay: 4.0,
+  }
 
   const gui = new GUI();
   const textureFolder = gui.addFolder('変形テクスチャ');
@@ -120,44 +127,98 @@ async function init(){
       reader.readAsDataURL(file);
     }
   });
-
   textureFolder.open();
 
-  //Progress
-  const tl = gsap.timeline({ repeat: -1 });
-  tl.to(uniforms.uProgress, {
-    value: 1.0,
-    duration: 0.8,
-    delay: 4,
-    ease: Circ.easeInOut,
-    onComplete: ()=>{
-      uniforms.uTexCurrent.value = texture02;
-      uniforms.uTexNext.value = texture03;
-      uniforms.uProgress.value = 0.0;
-    },
-  })
-  .to(uniforms.uProgress, {
-    value: 1.0,
-    duration: 0.8,
-    delay: 4,
-    ease: Circ.easeInOut,
-    onComplete: ()=>{
-      uniforms.uTexCurrent.value = texture03;
-      uniforms.uTexNext.value = texture01;
-      uniforms.uProgress.value = 0.0;
-    },
-  })
-  .to(uniforms.uProgress, {
-    value: 1.0,
-    duration: 0.8,
-    delay: 4,
-    ease: Circ.easeInOut,
-    onComplete: ()=>{
-      uniforms.uTexCurrent.value = texture01;
-      uniforms.uTexNext.value = texture02;
-      uniforms.uProgress.value = 0.0;
-    },
+
+  const gsapFolder = gui.addFolder('アニメーション設定');
+  gsapFolder.add(settings, 'duration', 0.1, 3.0, 0.1).name('変化秒数').onChange((value) => {
+    settings.duration = value;
+    updateTimeline();
   });
+  gsapFolder.add(settings, 'delay', 1.0, 8.0, 0.5).name('待機秒数').onChange((value) => {
+    settings.delay = value;
+    updateTimeline();
+  });
+  gsapFolder.open();
+
+  //Progress
+  // const tl = gsap.timeline({ repeat: -1 });
+  // tl.to(uniforms.uProgress, {
+  //   value: 1.0,
+  //   duration: duration,
+  //   delay: 4,
+  //   ease: 'power3.out',
+  //   onComplete: ()=>{
+  //     uniforms.uTexCurrent.value = texture02;
+  //     uniforms.uTexNext.value = texture03;
+  //     uniforms.uProgress.value = 0.0;
+  //   },
+  // })
+  // .to(uniforms.uProgress, {
+  //   value: 1.0,
+  //   duration: duration,
+  //   delay: 4,
+  //   ease: 'power2.inOut',
+  //   onComplete: ()=>{
+  //     uniforms.uTexCurrent.value = texture03;
+  //     uniforms.uTexNext.value = texture01;
+  //     uniforms.uProgress.value = 0.0;
+  //   },
+  // })
+  // .to(uniforms.uProgress, {
+  //   value: 1.0,
+  //   duration: duration,
+  //   delay: 4,
+  //   ease: Circ.easeInOut,
+  //   onComplete: ()=>{
+  //     uniforms.uTexCurrent.value = texture01;
+  //     uniforms.uTexNext.value = texture02;
+  //     uniforms.uProgress.value = 0.0;
+  //   },
+  // });
+
+  let timeline = gsap.timeline({ repeat: -1 });
+
+  function updateTimeline() {
+    timeline.kill(); // 既存のタイムラインを停止
+    timeline = gsap.timeline({ repeat: -1 });
+
+    timeline.to(uniforms.uProgress, {
+      value: 1.0,
+      duration: settings.duration,
+      delay: settings.delay,
+      ease: 'power2.inOut',
+      onComplete: ()=>{
+        uniforms.uTexCurrent.value = texture02;
+        uniforms.uTexNext.value = texture03;
+        uniforms.uProgress.value = 0.0;
+      },
+    })
+    .to(uniforms.uProgress, {
+      value: 1.0,
+      duration: settings.duration,
+      delay: settings.delay,
+      ease: 'power2.inOut',
+      onComplete: ()=>{
+        uniforms.uTexCurrent.value = texture03;
+        uniforms.uTexNext.value = texture01;
+        uniforms.uProgress.value = 0.0;
+      },
+    })
+    .to(uniforms.uProgress, {
+      value: 1.0,
+      duration: settings.duration,
+      delay: settings.delay,
+      ease: 'power2.inOut',
+      onComplete: ()=>{
+        uniforms.uTexCurrent.value = texture01;
+        uniforms.uTexNext.value = texture02;
+        uniforms.uProgress.value = 0.0;
+      },
+    });
+  }
+
+  updateTimeline();
 
 
   function animate(){
